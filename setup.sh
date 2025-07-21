e3f08292363d82d44b890098d3b75e76e505d88f
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-07-21 15:48:17 +0200

    add piquel user to docker group

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@
 # as root
 usermod --login piquel --move-home --home /home/piquel ubuntu
 groupmod --new-name piquel ubuntu
+usermod -a -G docker piquel
 curl -sSL https://dokploy.com/install.sh | sh
